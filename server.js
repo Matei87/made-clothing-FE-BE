e2401ec7aedacdbe2f386fc682903302e27df7e5
Matei Mircea <mrc_matei@yahoo.com>
2021-04-01 18:22:19 +0300

    changed package.json to heroku deploy

--- server.js
+++ server.js
@@ -33,10 +33,9 @@ app.use('/items', items);
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
